5346af14db28a5adebe55b19c3c01d5447a1bf46
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2013-03-21 19:49:57 +0100

    Refactors the LibraryDescriptionOptions

--- options.go
+++ options.go
@@ -1,27 +1,26 @@
 package main
 
 import (
-	"github.com/jessevdk/go-flags"
-	"os"
 	"fmt"
+	"github.com/jessevdk/go-flags"
 )
 
-
-type Options struct {
-	Version func() error `short:"V" long:"version" description:"Print the version"`
-
+type VersionCmd struct {
 }
 
-var options = &Options{
-	Version: func() error {
-		fmt.Println("easy-abi-check version ", version_string)
-		os.Exit(0)
-		return nil
-	},
+func (c *VersionCmd) Execute(args []string) error {
+	fmt.Println("easy-abi-check version ", version_string)
+	return nil
 }
 
-var parser = flags.NewParser(options, flags.Default)
+var opts = LibraryDescriptionOptions{}
+
+var parser = flags.NewParser(&opts, flags.Default)
 
 func init() {
 	parser.Description = "A wrapper around abi-compliance-check to automate C/C++ compliance check"
+	parser.AddCommand("version",
+		"prints the current version",
+		"prints the current version, simply.",
+		&VersionCmd{})
 }
